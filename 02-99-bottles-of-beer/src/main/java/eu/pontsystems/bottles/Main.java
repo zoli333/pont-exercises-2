6118f80f3dcb7dd5782253d7d193415afea14278
Zoltan Nagy <13023894+zoli333@users.noreply.github.com>
2018-06-15 18:35:47 +0200

    Update Main.java
    uncomment main method's calls

--- 02-99-bottles-of-beer/src/main/java/eu/pontsystems/bottles/Main.java
+++ 02-99-bottles-of-beer/src/main/java/eu/pontsystems/bottles/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		/*
 		System.out.println(BottlesOfBeer.sing());
-		
 		System.out.println("==============");
 		System.out.print(BottlesOfBeer.verses(25,0));
 		System.out.println("==============");
@@ -15,9 +13,11 @@ public class Main {
 		System.out.print(BottlesOfBeer.verse(25));
 		System.out.println("==============");
 		System.out.print(BottlesOfBeer.verse(0));
+		System.out.println("==============");
 		System.out.print(BottlesOfBeer.verse(1));
-		*/
+		System.out.println("==============");
 		System.out.print(BottlesOfBeer.verse(1));
+		System.out.println("==============");
 		
 	}
 }
